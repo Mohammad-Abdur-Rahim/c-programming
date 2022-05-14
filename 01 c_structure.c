65ec616720ad529d56e666e956d3e8d7a4b4b23c
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2022-05-14 20:22:23 +0600

    Basic Structure C_program

---
+++ 01 c_structure.c
@@ -0,0 +1,11 @@
+#include<stdio.h>                                               // Standard Input Output. Header file
+int main()
+{
+    printf("Journey By C_programming\n");
+    printf("Md Abdur Rahim\n");
+    printf("Daffodil International University\n");
+    printf("01995516880");                                     // Output libray function & Escape sequence
+    getch ();                                                             // Output libray function & Escape sequence
+    return 0;
+
+}
