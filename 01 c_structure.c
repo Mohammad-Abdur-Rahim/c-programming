66449f33ef4c25823652bcbfd66c04ac939bbf4c
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2022-05-15 21:17:36 +0600

    write a two output Integer with Scanf function

--- 01 c_structure.c
+++ 01 c_structure.c
@@ -1,13 +1,11 @@
-// Write a program how to input type any user.
-#include<stdio.h>
+// Write a program how to printout two user input
 int main()
 {
 
-    int num;
-
-    printf("please click your roll number is : ");
-    scanf("%d",&num);
-    printf("Your roll number is = %d\n",num);
+    int num1,num2;
+    printf("Enter two Integers: ");
+    scanf("%d %d",&num1,&num2);
+    printf("Number's are : %d ,%d",num1,num2);
 
     return 0;
 
