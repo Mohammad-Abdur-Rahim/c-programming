6f78e35daff9c9b2e75d043a50d2358c6038008d
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2022-05-15 21:45:06 +0600

    ASCII value Convert Character

---
+++ 02 Convert ASCII value.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+
+    int num;
+    printf("Please inter any ASCII value :");
+    scanf("%d",&num);
+
+    printf("Your ASCII value Character is : %c\a",num);
+
+
+
+    return 0;
+}
