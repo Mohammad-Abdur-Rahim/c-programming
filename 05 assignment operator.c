65ee0bb1b32b23cd55c1239dc80081990ccd148b
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2022-05-18 19:34:38 +0600

    Sum of Integer

---
+++ 05 assignment operator.c
@@ -0,0 +1,13 @@
+//Write a program thats print a sum
+#include<stdio.h>
+int main()
+{
+    int num1,num2,sum;
+
+    printf("Enter two numbers: ");
+    scanf("%d%d",&num1,&num2);
+
+    sum=num1 + num2;
+
+    printf("Sum is :%d",sum);
+}
