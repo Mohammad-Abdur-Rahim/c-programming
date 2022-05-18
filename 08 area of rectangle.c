b1be2c12f2bfe5c24d7b9c866157ce9a41031c06
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2022-05-18 22:31:59 +0600

    Area of Rectangle

---
+++ 08 area of rectangle.c
@@ -0,0 +1,23 @@
+// Area of Rectangle = Length * Width
+#include<stdio.h>
+int main()
+{
+
+float length,width,area;
+
+printf("Enter Length : ");
+scanf("%f",&length);
+
+printf("Enter Width : ");
+scanf("%f",&width);
+
+area = length * width ;
+printf("Area is Rectangle : %.2f",area);
+
+
+
+
+
+    getch();
+    return 0;
+}
