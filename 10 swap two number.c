9cc711a126749afb0b8fef0297305dfc40801647
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2022-05-19 19:54:09 +0600

    Swap two number without variables

--- 10 swap two number.c
+++ 10 swap two number.c
@@ -1,15 +1,16 @@
-//Swap two numbers with variable
+//Swap two numbers without variable
 #include<stdio.h>
 int main()
 {
-    int num1=10,num2=20,temp;
 
-    temp=num1;
-    num1=num2;
-    num2=temp;
+    int num1=10,num2=20;
 
-    printf("Number one is = %d\n\a",num1);
-    printf("Number two is = %d",num2);
+num1= num1-num2;
+num2=num1+num2;
+num1=num2-num1;
+
+printf("Number one is :%d\n\a",num1);
+printf("Number two is :%d",num2);
 
 
     getch();
