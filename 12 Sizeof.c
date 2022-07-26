0d2fee5aaab6c2213b162fdba4ea52cb0ea49f00
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2022-07-26 20:36:42 +0600

    Size of Bytes

---
+++ 12 Sizeof.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    float f;
+    double d;
+    char c;
+    printf("Size of int=%d Bytes\n",sizeof(i));
+    printf("Size of int=%d Bytes\n",sizeof(f));
+    printf("Size of int=%d Bytes\n",sizeof(d));
+    printf("Size of int=%d Bytes\n",sizeof(c));
+    return 0;
+
+}
