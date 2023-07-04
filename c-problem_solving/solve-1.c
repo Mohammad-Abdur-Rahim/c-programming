15dbd507c3182e804f828a6a762c958c302a0051
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2023-07-04 18:24:20 +0600

    new 2023 july

---
+++ c-problem_solving/solve-1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    char s[900];
+    scanf("%[^\n]s",&s);         /* [^\n] important keyword this code*/
+
+         printf("Hello, World!\n");
+         printf("%s",s);
+
+    return 0;
+}
