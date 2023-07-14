31c2644bf88159be63eab1b52bc29224656b9811
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2023-07-14 06:59:37 +0600

    Integer & Long Interger class 02 amazon vai

--- 28. Practise.c
+++ 28. Practise.c
@@ -2,12 +2,13 @@
 int main()
 {
 
+int a=2147483647;
+//int  b=2147483648;
+long long int  b=2147483648;
 
-printf("My salary is %d:\n",34000);
-printf("%d+%d=%d\n",10,20,30);
-printf("The has character %c\n",'#');
-printf("My Height is %.1f And my weight is %d kg",5.6,55);
-
+printf("Result is %d\n",a);
+//  printf("Result is %d\n",b);     value limit cross 4bit
+printf("Result is %lld\n",b);
 
 
 
