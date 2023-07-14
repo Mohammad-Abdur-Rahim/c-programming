511891ec24bc9a1daaa8bd7a7fc6db37b0398e78
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2023-07-14 06:47:28 +0600

    Data type

---
+++ 28. Practise.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+
+
+printf("My salary is %d:\n",34000);
+printf("%d+%d=%d\n",10,20,30);
+printf("The has character %c\n",'#');
+printf("My Height is %.1f And my weight is %d kg",5.6,55);
+
+
+
+
+    return 0;
+}
